126bcb6958e8d833aa08945a462965ce39df9407
Christophe Thibaut <cthibaut@octo.com>
2020-09-03 18:39:25 +0200

    extract method calc-bonus

--- bowling.fs
+++ bowling.fs
@@ -4,6 +4,10 @@
 : COLLECT-BONUS ( score,switch,throw1 -- score )
   * + ;
 
+: CALC-BONUS ( throw1,throw2 -- switch )
+  + 10 = 
+  IF 1 ELSE 0 THEN ;
+
 \ given the current bonus switch and score, and two rolls,
 \ calculate the current score. All the data is on the stack
 
@@ -12,11 +16,10 @@
   SWAP           \ score, switch
   R@             \ score, switch, throw1   - R throw2, throw1
   COLLECT-BONUS  \ score
-  R> R>          \ new-score, throw1, throw2  
-  OVER OVER      \ new-score, throw1, throw2, throw1, throw2
-  + 10 = NEGATE  \ new-score, throw1, throw2, (0|1)
-  -ROT           \ new-score, (0|1), throw1, throw2
-  +              \ new-score, (0|1), frame-score
-  ROT            \ (0|1), frame-score, new-score
-  +              \ (0|1), frame-total  
+  2R@            \ score', throw1, throw2  
+  CALC-BONUS     \ score', switch
+  R> R>          \ score', switch, throw1, throw2
+  +              \ score', switch, frame-score
+  ROT            \ switch, frame-score, score'
+  +              \ switch, score'  
   ;
